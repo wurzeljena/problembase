3e8dfcbd412a0c283c276f59ef486b11c7f7fe88
Aaron Puchert <aaronpuchert@alice-dsl.net>
2013-03-22 14:53:50 +0100

    Redesigned header
    Adapt coloring and popup login form for narrow screens.

--- head.php
+++ head.php
@@ -12,26 +12,28 @@
 		<a href="user.php"><i class="icon-group"></i> <span>Benutzerliste</span></a>
 </li><li>
 		<a href="tagpanel.php?standalone"><i class="icon-tags"></i> <span>Tag-Editor</span></a>
+</li><?php	endif;
+		if ($id == "headermenu"): ?><li>
+		<a href="javascript:Login.Show();"><i class="icon-signin"></i></a>
 </li><?php endif; ?>
 	</ul>
 	</nav>
 <?php } ?>
 <div class="head"><div class="center">
-	<span class="logo">
-		<a href="index.php">&#x221A;<span style="text-decoration:overline">WURZEL</span></a>
-	</span>
+	<a id="logo" href="index.php">&#x221A;<span style="text-decoration:overline">WURZEL</span></a>
 	<?php drawMenu("headermenu"); ?>
-	<form class="login" action="logon.php" method="POST">
+	<form id="login" action="logon.php" method="POST">
 	<input type="hidden" name="referer" value="<?php print $_SERVER['REQUEST_URI']; ?>">
 	<?php if (!isset($user_id)) { ?>
 		<input type="email" style="width:15em;" name="email" placeholder="E-Mail">
 		<input type="password" name="password" placeholder="Passwort">
 		<input type="submit" value="Login">
-	<?php } else { ?>
-		Sie sind angemeldet als: <?php print $_SESSION['user_name']; ?>
+	<?php } else {
+			print "<span id='username'>{$_SESSION['user_name']}</span>";
+		?>
 		<input type="hidden" name="logout" value="1">
 		<input type="submit" value="Logout">
 	<?php } ?>
 	</form>
-	<div style="font-family:sans-serif; font-size:x-small;">Aufgabendatenbank <br /> &copy; 2012 <a href="http://www.wurzel.org/" target="_blank">Wurzel e.V.</a></div>
+	<script>var Login = new PopupTrigger("login");</script>
 </div></div>
