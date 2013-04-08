197f6812078e876fb31b2f1914dce4467d8b955d
Aaron Puchert <aaronpuchert@alice-dsl.net>
2013-04-08 21:04:24 +0200

    Redesigned filter form without tables

--- index.php
+++ index.php
@@ -28,34 +28,35 @@
 		<div><input type="text" name="filter" placeholder="Suchbegriff"
 			value="<?php if (isset($_REQUEST['filter'])) print $_REQUEST['filter']; ?>"/>
 		<input type="submit" value="Filtern"></div>
-		<table style="border-top:1px solid Gray; border-bottom:1px solid Gray;">
-			<tr>
-				<td><label class="question" for="proposer">Wer?</label></td>
-				<td><input type="text" name="proposer" id="proposer" placeholder="Autor" list="proposers"
-					value="<?php if (isset($_REQUEST['proposer'])) print $_REQUEST['proposer']; ?>"/> </td>
-				<?php proposers_datalist($pb); ?>
-			</tr>
-			<tr>
-				<td><label class="question" for="evaluation">Wie?</label></td>
-				<td><span id="evaluation">{Bewertungsbereich}</span></td>
-			</tr>
-			<tr>
-				<td><label class="question" for="number">Wann?</label></td>
-				<td><input type="text" name="number" id="number" placeholder="MM/JJ" style="width:45px;" pattern="([1-9]|0[1-9]|1[0-2])/[0-9]{2}"
-					value="<?php if (isset($_REQUEST['number'])) print $_REQUEST['number']; ?>"/>
-				<input type="checkbox" name="with_solution" id="with_solution" <?php if (isset($_REQUEST['with_solution'])) print "checked"; ?>/><label class="info" for="with_solution">mit L&ouml;sung</label></td>
-			</tr>
-			<tr>
-				<td><label class="info" for="start">nach</label></td>
-				<td><input type="date" name="start" id="start" placeholder="JJJJ-MM-TT" pattern="[0-9]{4}-(0[1-9]|1[012])-(0[1-9]|1[0-9]|2[0-9]|3[01])"
-					value="<?php if (isset($_REQUEST['start'])) print $_REQUEST['start']; ?>"/></td>
-			</tr>
-			<tr>
-				<td><label class="info" for="end">vor</label></td>
-				<td><input type="date" name="end" id="end" placeholder="JJJJ-MM-TT" pattern="[0-9]{4}-(0[1-9]|1[012])-(0[1-9]|1[0-9]|2[0-9]|3[01])"
-					value="<?php if (isset($_REQUEST['end'])) print $_REQUEST['end']; ?>"/></td>
-			</tr>
-		</table>
+		<div id="questions">
+		<div>
+			<label class="question" for="proposer">Wer?</label>
+			<input type="text" name="proposer" id="proposer" placeholder="Autor" list="proposers"
+				value="<?php if (isset($_REQUEST['proposer'])) print $_REQUEST['proposer']; ?>"/>
+			<?php proposers_datalist($pb); ?>
+		</div>
+		<div>
+			<label class="question" for="evaluation">Wie?</label>
+			<span id="evaluation">{Bewertungsbereich}</span>
+		</div>
+		<div>
+			<label class="question" for="number">Wann?</label>
+			<input type="text" name="number" id="number" placeholder="MM/JJ" style="width:45px;"
+				pattern="([1-9]|0[1-9]|1[0-2])/[0-9]{2}" value="<?php if (isset($_REQUEST['number'])) print $_REQUEST['number']; ?>"/>
+			<input type="checkbox" name="with_solution" id="with_solution" <?php if (isset($_REQUEST['with_solution'])) print "checked"; ?>/>
+				<label class="info" for="with_solution">mit L&ouml;sung</label>
+		</div>
+		<div>
+			<label class="info" for="start">nach</label>
+			<input type="date" name="start" id="start" placeholder="JJJJ-MM-TT" pattern="[0-9]{4}-(0[1-9]|1[012])-(0[1-9]|1[0-9]|2[0-9]|3[01])"
+				value="<?php if (isset($_REQUEST['start'])) print $_REQUEST['start']; ?>"/>
+		</div>
+		<div>
+			<label class="info" for="end">vor</label>
+			<input type="date" name="end" id="end" placeholder="JJJJ-MM-TT" pattern="[0-9]{4}-(0[1-9]|1[012])-(0[1-9]|1[0-9]|2[0-9]|3[01])"
+				value="<?php if (isset($_REQUEST['end'])) print $_REQUEST['end']; ?>"/>
+		</div>
+		</div>
 		<div class="taglist">
 			<label class="question" for="tag_select">Was?</label>
 			<?php tag_select($pb, "filter"); ?>
